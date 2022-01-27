f42a4e65414fd4ebf9a6e3a19e7cdf716184df66
Leon Jungemeyer <leon.jungemeyer@me.com>
2022-01-27 11:58:14 +0100

    Execute alg comp in parallel

--- slurm/start_alg_comparison.sh
+++ slurm/start_alg_comparison.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-#SBATCH --ntasks=1
-#SBATCH --partition=single
-#SBATCH --mem=16000
-#SBATCH --time=8:00:00
-#SBATCH --parsable
-
-# Testing PLATIPUS with params from the paper
-# Not many hyperparams provided, assumed to be similar to MAML
 
 echo 'COMPARISON started'
 
@@ -23,23 +15,6 @@ for benchmark in Sinusoid1D
 do
     for alg in baseline maml platipus bmaml
     do
-        python train.py --algorithm $alg \
-                        --wandb True \
-                        --num_epochs 50000 \
-                        --benchmark $benchmark \
-                        --nlml_testing_enabled True \
-                        --num_models 10 \
-                        --k_shot 5 \
-                        --num_inner_updates 1 \
-                        --seed 5544 \
-                        --inner_lr 0.01 \
-                        --meta_lr 0.001 \
-        	        --minibatch 25 \
-                        --num_episodes_per_epoch 25 \
-                        --noise_stddev 0.02 \
-                        --num_hidden 2 \
-                        --hidden_size 40 \
-                        --num_episodes 4 \
-                        --logdir_base /pfs/work7/workspace/scratch/utpqw-meta
+        sbatch alg_comparison.sh ALGO=$alg
     done
 done
