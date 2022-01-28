corrected nlml calculation and added mse calculation on test set.

bayesian_meta_learning/MainRunner.py
@@ -1,6 +1,6 @@
 import os
 from bayesian_meta_learning import visualizer
-from bayesian_meta_learning.nlml_tester import test_neg_log_marginal_likelihood
+from bayesian_meta_learning.model_tester import calculate_loss_metrics
 from bayesian_meta_learning.benchmark.benchmark_dataloader import create_benchmark_dataloaders
 import wandb
 import numpy as np
@@ -29,9 +29,9 @@ class MainRunner():
         }
         self.algo = algorithms[config['algorithm']](config)
         if self.config['wandb']:
-            wandb.define_metric(name="nlml_test/run_id")
-            wandb.define_metric(name="nlml_test/*",
-                                step_metric="nlml_test/run_id")
+            wandb.define_metric(name="evaluation/run_id")
+            wandb.define_metric(name="evaluation/*",
+                                step_metric="evaluation/run_id")
         return
 
     def run(self) -> None:
@@ -57,14 +57,16 @@ class MainRunner():
             if self.config['algorithm'] == 'platipus':
                 self._test_platipus(test_dataloader)
             else:
-                nlml = test_neg_log_marginal_likelihood(
+                nlml, mse = calculate_loss_metrics(
                     self.algo, test_dataloader, self.config)
                 if(self.config['wandb']):
                     wandb.log({
-                        'nlml_test/run_id': 1,
-                        'nlml_test/loss': nlml
+                        'evaluation/run_id': 1,
+                        'evaluation/nlml': nlml,
+                        'evaluation/mse': mse
                     })
-                print(f"Test-NLML: {nlml} \n")
+                print(f"Test-NLML: {nlml}")
+                print(f"Test-MSE: {mse}")
 
         # # visualize the dataset (training, validation and testing)
         print("Visualization is started.")
@@ -87,27 +89,38 @@ class MainRunner():
 
     def _test_platipus(self, test_dataloader):
         old_num_models = self.config['num_models']
-        num_runs = 100
-        means = []
-        stds = []
-        num_models_array = [5, 10, 20, 50, 100]
-        for num_models in num_models_array:
-            nlml = [float] * num_runs
+        num_runs = 10
+        num_models_array = [5, 10]#, 20, 50, 100]
+        nlmls = np.zeros((len(num_models_array), num_runs))
+        mses = np.zeros((len(num_models_array), num_runs))
+        print('{:<10} {:<10} {:<15} {:<15}'.format(
+            'num_models', 'run_id', 'NLML', 'MSE'))
+        for model_id, num_models in enumerate(num_models_array):
             for run_id in range(num_runs):
                 self.algo.config['num_models'] = num_models
                 self.config['num_models'] = num_models
-                nlml[run_id] = test_neg_log_marginal_likelihood(
+                nlml, mse = calculate_loss_metrics(
                     self.algo, test_dataloader, self.config)
                 if(self.config['wandb']):
                     wandb.log({
-                        'nlml_test/run_id': run_id,
-                        f'nlml_test/loss_num_models_{num_models}': nlml[run_id]
+                        'evaluation/run_id': run_id,
+                        f'evaluation/nlml_num_models_{num_models}': nlml,
+                        f'evaluation/mse_num_models_{num_models}': mse
                     })
-                print(f"{num_models}_{run_id+1}: {np.round(nlml[run_id], 4)}")
-            means.append(np.round(np.mean(nlml), 4))
-            stds.append(np.round(np.std(nlml), 4))
-        for i, num_models in enumerate(num_models_array):
-            print(f"Test-NLML_{num_models}: {means[i]} +- {stds[i]}")
+                print('{:<10} {:<10} {:<15} {:<15}'.format(num_models, run_id+1,
+                      np.round(nlml, 4), np.round(mse, 4)))
+                nlmls[model_id, run_id] = nlml
+                mses[model_id, run_id] = mse
+        print('\nAverage and std_dev over all runs')
+        print('{:<10} {:<30} {:<30}'.format('num_models', 'NLML', 'MSE'))
+        for model_id, num_models in enumerate(num_models_array):
+            nlml = np.round(np.mean(nlmls[model_id]), 4)
+            nlml_std = np.round(np.std(nlmls[model_id]), 4)
+            mse = np.round(np.mean(mses[model_id]), 4)
+            mse_std = np.round(np.std(mses[model_id]), 4)
+            nlml_string = f'{nlml} +- {nlml_std}'
+            mse_string = f'{mse} +- {mse_std}'
+            print('{:<10} {:<30} {:<30}'.format(num_models, nlml_string, mse_string))
         print("")
         # restore num_models
         self.algo.config['num_models'] = old_num_models

bayesian_meta_learning/model_tester.py
@@ -4,26 +4,28 @@ from torch.utils.data.dataloader import DataLoader
 from torch import Tensor
 from typing import Tuple
 import numpy as np
-import wandb
 
 
-def test_neg_log_marginal_likelihood(algo, test_dataloader: DataLoader, config: dict) -> float:
+def calculate_loss_metrics(algo, test_dataloader: DataLoader, config: dict) -> Tuple[float, float]:
     model = algo.load_model(
         resume_epoch=config["num_epochs"], hyper_net_class=algo.hyper_net_class, eps_dataloader=test_dataloader)
-    nlml_per_task = torch.zeros((test_dataloader.dataset.n_tasks))
     y_pred, y_test = _predict_all_tasks(algo, model, test_dataloader, config)
-    N = y_pred.shape[2]
-    S = y_pred.shape[1]
-    noise_var = config['noise_stddev']**2
-    #constant = N/2*np.log(2*np.pi*noise_var) + np.log(S)
-    constant = np.log(S)
-    for task_index in range(test_dataloader.dataset.n_tasks):
-        exponent = torch.norm(y_pred[task_index] - y_test[task_index],
-                              dim=1)**2 / (2*noise_var)
-        nlml_per_task[task_index] = constant - \
-            torch.logsumexp(-exponent, dim=0)
-    nlml = torch.mean(nlml_per_task).item()
-    return nlml
+    nlml = _calculate_neg_log_marginal_likelihood(
+        y_pred, y_test, torch.tensor(config['noise_stddev']))
+    mse = torch.nn.MSELoss()
+    mse_loss = mse(y_pred, y_test).item()
+    return nlml, mse_loss
+
+
+def _calculate_neg_log_marginal_likelihood(y_pred: torch.Tensor, y_test: torch.Tensor, noise_stddev: torch.Tensor) -> float:
+    T = y_pred.shape[0]  # num tasks
+    S = y_pred.shape[1]  # num model samples
+    N = y_pred.shape[2]  # num datapoints
+    gaussian = torch.distributions.Normal(y_pred, noise_stddev)
+    log_prob = torch.sum(gaussian.log_prob(y_test), dim=2)
+    nlml_per_task = (np.log(S) - torch.logsumexp(log_prob, dim=1)) / N
+    nlml = torch.sum(nlml_per_task) / T
+    return nlml.item()
 
 
 def _predict_all_tasks(algo, model, task_dataloader, config: dict) -> Tuple[Tensor, Tensor]: