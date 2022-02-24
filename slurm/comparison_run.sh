99901022c8fcd71263d938830f5129c55194b220
Luis Augenstein <luis.augenstein@web.de>
2022-02-24 13:45:05 +0100

    removed platipus inner_lr=0.01 run and only use inner_lr = 0.001

--- slurm/comparison_run.sh
+++ slurm/comparison_run.sh
@@ -20,25 +20,6 @@ sbatch slurm/start_job.sh   algorithm=maml \
 				            num_episodes=4 \
 				            num_inner_updates=5 \
 
-
-sbatch slurm/start_job.sh   algorithm=platipus \
-                            num_epochs=10 \
-                            num_episodes_per_epoch=60000 \
-                            epochs_to_save=1 \
-                            benchmark=$benchmark \
-                            num_models=10 \
-                            k_shot=$k_shot \
-                            num_points_per_train_task=50 \
-                            inner_lr=0.01 \
-                            meta_lr=0.001 \
-                            minibatch=25 \
-                            noise_stddev=0.1 \
-                            num_hidden=3 \
-                            hidden_size=40 \
-                            num_episodes=4 \
-                            num_inner_updates=5 \
-                            KL_weight=0.01
-
 sbatch slurm/start_job.sh   algorithm=platipus \
                             num_epochs=10 \
                             num_episodes_per_epoch=60000 \
