f4cb912336b1c0b5edc53fb18766dd474194d118
Luis Augenstein <luis.augenstein@web.de>
2022-03-06 19:40:13 +0100

    changed parameter bounds for affine task

--- bayesian_meta_learning/benchmark/sinusoid_affine_benchmark.py
+++ bayesian_meta_learning/benchmark/sinusoid_affine_benchmark.py
@@ -2,6 +2,24 @@ import numpy as np
 from metalearning_benchmarks.benchmarks.base_benchmark import MetaLearningBenchmark, MetaLearningTask
 from metalearning_benchmarks import Sinusoid, Affine1D
 
+class CustomAffine1D(Affine1D):
+    m_bounds = np.array([-2, 2])
+    b_bounds = np.array([-2.5, 2.5])
+    param_bounds = np.array([m_bounds, b_bounds])
+    x_bounds = np.array([[-5.0, 5.0]])
+
+    def __init__(
+        self, n_task, n_datapoints_per_task, output_noise, seed_task, seed_x, seed_noise
+    ):
+        super().__init__(
+            n_task=n_task,
+            n_datapoints_per_task=n_datapoints_per_task,
+            output_noise=output_noise,
+            seed_task=seed_task,
+            seed_x=seed_x,
+            seed_noise=seed_noise,
+        )
+
 class SinusoidAffineBenchmark(MetaLearningBenchmark):
     d_param = 2
     d_x = 1
@@ -17,7 +35,7 @@ class SinusoidAffineBenchmark(MetaLearningBenchmark):
         self.n_affines = n_task - self.n_sinusoids
         self.sinusoid = Sinusoid(
             self.n_sinusoids, n_datapoints_per_task, output_noise, seed_task, seed_x, seed_noise)
-        self.affine1D = Affine1D(self.n_affines, n_datapoints_per_task,
+        self.affine1D = CustomAffine1D(self.n_affines, n_datapoints_per_task,
                                  output_noise, seed_task, seed_x, seed_noise)
         rng = np.random.RandomState(seed_task)
         self.permute = rng.permutation(n_task)
